f4c55fc20f8c71a83ec16cb28201d5f9337f8e84
Belek <email@example.com>
2024-10-26 23:11:44 +0600

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,90 @@
+import { Grade } from "@mui/icons-material";
+
+export const Bio = {
+  name: "Akeev Belek",
+  roles: [,
+    "Frontend Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/kvsujann",
+  insta: "https://www.instagram.com/kvujann/",
+  facebook: "https://www.facebook.com/profile.php?id=100090210256339",
+};
+
+export const skills = [
+  {
+    title: "General",
+    skills: [
+      {
+        name: "React",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {  
+    img : "https://headlight.news/wp-content/uploads/2024/04/lamborghini-urus-se.jpg",
+    id: 0,
+    role: "Web Develop",
+    company: "Personal",
+    date: "June 2024 - July 2024",
+    desc: "First project and first introduction to web development. ",
+    skills: [
+      "HTML",
+      "CSS",
+    ],
+  },
+  {
+    img : "https://emilyserven.net/assets/images/blog/ropasci.png",
+    id: 0,
+    role: "Rocks Paper Scissors",
+    company: "SU IT",
+    date: "June 2024 - Recently",
+    desc: "First project on the Python",
+    skills: [
+      "PYTHON",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0tlEXA73d8c_m5yzLodmli__vZzoS9CvHVQ&s",
+    school: "Salymbekov University",
+    date: "Oct 10, 2024",
+    desc: "I am currently studying at Salymbekov University in the Faculty of Computer Science. I strive to excel in my studies to gain a deeper understanding of technologies and programming languages. In the future, I aim to become a full stack developer, successfully implementing projects at all stages of development, from frontend to backend.",
+    degree: "IT and Businnes",
+    grade: 'middle'
+  },
+];
+
